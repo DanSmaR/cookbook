c463299a91352f0e04244f305e0dcbfd7081078f
Danilo Martins <dansm86@outlook.com>
2023-12-12 00:11:23 -0300

    test:editar receitas de outros users

--- spec/features/user_edit_recipe_spec.rb
+++ spec/features/user_edit_recipe_spec.rb
@@ -30,4 +30,22 @@ feature 'Usuário edita receita' do
 
     expect(current_path).to eq new_user_session_path
   end
+
+  scenario 'não tem autorização para editar receitas de outros usuários' do
+    user = create(:user, email: 'user@email.com', password: '123456', role: :user)
+    another_user = create(:user, email: 'otheruser@email.com', password: '123456', role: :user)
+    create(:recipe_type, name: 'Lanche')
+    recipe_type = create(:recipe_type, name: 'Sobremesa')
+    recipe = create(:recipe, user: user, recipe_type: recipe_type)
+
+    login_as another_user, scope: :user
+    visit recipe_path(recipe)
+
+    expect(page).to_not have_link('Editar')
+
+    visit edit_recipe_path(recipe)
+
+    expect(current_path).to eq recipe_path(recipe)
+    expect(page).to have_content('Não possui autorização para editar essa receita')
+  end
 end
