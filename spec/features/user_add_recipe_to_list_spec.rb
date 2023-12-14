fcd831cd5380bbb5709176914db228a162a72998
Danilo Martins <dansm86@outlook.com>
2023-12-14 14:45:38 -0300

    test:link recipes list at menu

--- spec/features/user_add_recipe_to_list_spec.rb
+++ spec/features/user_add_recipe_to_list_spec.rb
@@ -121,4 +121,35 @@ feature 'Usuario vê Listas para escolher' do
     expect(page).to have_content 'Não foi possível criar lista. Nome não pode ficar em branco'
     expect(page).to have_select 'Selecione a Lista', options: %w[Natal Fit]
   end
+
+  scenario 'e acessa a relação de listas pelo menu' do
+    user = create(:user, email: 'user@email.com', password: '123456', role: :user)
+
+    recipe_type = create(:recipe_type)
+    recipe2 = create(:recipe, title: 'Manjar', cook_time: 60,
+                     recipe_type:,
+                     ingredients: 'leite condensado, leite, leite de coco',
+                     instructions: 'Misture tudo, leve ao fogo e mexa, leve a geladeira')
+    recipe_type = create(:recipe_type, name: 'Lanche')
+    recipe1 = create(:recipe, title: 'Hamburguer', cook_time: 10,
+                     recipe_type:,
+                     ingredients: 'hamburguer, pão de hamburguer, queijo',
+                     instructions: 'Frite o hamburguer, coloque no pão, coma')
+    recipe3 = create(:recipe, title: 'HotDog', cook_time: 10,
+                     recipe_type: ,
+                     ingredients: 'salsicha, pão de hotdog, ketchup, mostarda',
+                     instructions: 'Asse a salsicha, coloque no pão, coma')
+
+    user.lists.create!(:name => 'Natal')
+    user.lists.create!(:name => 'Fit')
+
+    user.lists.first&.recipes << recipe1
+    user.lists.first&.recipes << recipe2
+    user.lists.first&.recipes << recipe3
+
+    login_as user, scope: :user
+    visit root_path
+
+    expect(page).to have_link 'Listas de Receitas', href: recipe_lists_path
+  end
 end
