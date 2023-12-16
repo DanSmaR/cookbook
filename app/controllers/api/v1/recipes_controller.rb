c930effa994e2e2b1496ba2fbc5fcc645c6b39ac
Danilo Martins <dansm86@outlook.com>
2023-12-16 13:37:02 -0300

    feat:get "/api/v1/recipes?title=a

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,6 +2,11 @@ class Api::V1::RecipesController < ActionController::API
   def index
     recipes = Recipe.all
 
+    unless params[:title].nil?
+      recipes = recipes.where('title LIKE ?',
+                              "%#{Recipe.sanitize_sql_like(params[:title])}%")
+    end
+
     render json: recipes.as_json(only: [:id, :title, :cook_time],
                                  include: {
                                    recipe_type: { only: [:id, :name] },
